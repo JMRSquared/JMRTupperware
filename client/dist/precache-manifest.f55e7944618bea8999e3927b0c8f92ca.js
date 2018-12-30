237bb5bcaf6fa2b64dbe5e88761e1a5ec508f575
Sirwali Joseph <mulavhe@gmail.com>
2018-12-30 19:07:38 +0200

    Build client for ALPHA

---
+++ client/dist/precache-manifest.f55e7944618bea8999e3927b0c8f92ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5279e58d20f5a1ab3e8f",
+    "url": "/js/chunk-vendors.f63c1a01.js"
+  },
+  {
+    "revision": "8cec00376cc59d2a9b5c",
+    "url": "/js/app.c28e8026.js"
+  },
+  {
+    "revision": "39285ea4fdd46c4dc6c9",
+    "url": "/js/about.af146977.js"
+  },
+  {
+    "revision": "7a47d740152bb019c837b38be4d475a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07e2b18d7d246b40eda6d56fa29ffdff",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "07e2b18d7d246b40eda6d56fa29ffdff",
+    "url": "/img/logo.07e2b18d.png"
+  },
+  {
+    "revision": "5279e58d20f5a1ab3e8f",
+    "url": "/css/chunk-vendors.bd85b853.css"
+  },
+  {
+    "revision": "8cec00376cc59d2a9b5c",
+    "url": "/css/app.eff42480.css"
+  }
+];
